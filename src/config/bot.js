0d9734d657fb2e41731ca541dd44d3c731f954c8
fhm2709 <fahimtoplay@hotmail.com>
2026-08-15 22:15:54 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -372,7 +372,7 @@ export const botConfig = {
     // Welcome template posted when a user joins.
     // Placeholders: {user}, {server}, {memberCount}
     defaultWelcomeMessage:
-      "Welcome {user} to {server}! We now have {memberCount} members!",
+      "parle ou t une slp {user}!",
     // Goodbye template posted when a user leaves.
     // Placeholders: {user}, {memberCount}
     defaultGoodbyeMessage:
