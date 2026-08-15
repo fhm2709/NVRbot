c0f36ac59147edc43915f5fe7045096a56a0cb7c
fhm2709 <fahimtoplay@hotmail.com>
2026-08-15 19:57:09 +0200

    Update activity type description in bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: "No Scrubs", // required by Discord API, not shown in the client
         state: "No Scrubs",     // this is what people actually see
-        type: 2,               // Grand Theft Auto VI
+        type: 2,               // custom
       },
     ],
   },
